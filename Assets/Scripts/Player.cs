125968c76e6298777bbaace16064a2171ab33299
Jason Stepp <info@jasonstepp.me>
2020-09-19 14:10:37 -0700

    Init

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] float moveSpeed = 1f;
+    public float pixPerUnit = 16;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetAxisRaw("Horizontal") != 0){
+            transform.position = new Vector2(transform.position.x + Input.GetAxisRaw("Horizontal") * moveSpeed * Time.deltaTime, transform.position.y);
+        }
+
+        if(Input.GetAxisRaw("Vertical") != 0){
+            transform.position = new Vector2(transform.position.x, transform.position.y + Input.GetAxisRaw("Vertical") * moveSpeed * Time.deltaTime);
+        }
+
+    }
+}
